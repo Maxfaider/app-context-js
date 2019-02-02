e4ff969f677339661df39fdc91207495cd1a4319
Alan M.E <alanmarquez@outlook.com>
2019-02-02 16:26:50 -0500

    changed names interface

--- test/Appcontext.test.js
+++ test/Appcontext.test.js
@@ -3,45 +3,39 @@ var AppContext = require('../lib/app-context.js');
 
 describe('Test: module AppContext', function() {
 
-    describe('Object bindings global', function() {
+    it('Object bindings global', function() {
         var nameApp = 'fumoffu-app';
         var person = { name: 'Alan M.E', lastnames: 'M.E' }
 
-        AppContext.addAttr('nameApp', nameApp);
-        AppContext.addAttr('person', person);
+        AppContext.setAttribute('nameApp', nameApp);
+        AppContext.setAttribute('person', person);
 
-        it('attributes existent', function() {
-            assert.strictEqual(AppContext.getAttr('nameApp'), nameApp);
-            assert.strictEqual(AppContext.getAttr('person'), person);
-        });
+        assert.strictEqual(AppContext.getAttribute('nameApp'), nameApp);
+        assert.strictEqual(AppContext.getAttribute('person'), person);
     });
 
-    describe('Publish Service', function() {
+    it('Publish Service: Facade', function() {
         var api = { api: "some functions"};
-
         AppContext.Services.publish('registro_User', api);
-
-        it('services existent', function() {
-            assert.strictEqual(AppContext.Services.getService('registro_User'), api);
-        });
+        
+        assert.strictEqual(AppContext.Services.getService('registro_User').api, api);
     });
 
-    describe('Publish Services', function() {
+    it('Publish Services: Facade', function() {
         var api_admin = { api: "some admin api"};
         var api_task = { api: "some task api"};
 
-        AppContext.Services.publish('registro_Admin', api_admin);
-        AppContext.Services.publish('registro_Task', api_task);
+        AppContext.Services.publish('registerAdmin', api_admin);
+        AppContext.Services.publish('registerTask', api_task);
 
-        var result = AppContext.Services.getServices(['registro_Admin', 'registro_Task']);
-        it('services existent', function() {
-            assert.equal(result.registro_Admin, api_admin);
-            assert.equal(result.registro_Task, api_task);
-            assert.equal(AppContext.Services.size(), 3); // registro_Admin, registro_Task, registro_User
-        });
+        var result = AppContext.Services.getServices(['registerAdmin', 'registerTask']);
+
+        assert.strictEqual(result.registerAdmin.api, api_admin);
+        assert.strictEqual(result.registerTask.api, api_task);
+        assert.strictEqual(AppContext.Services.size(), 3); // registro_Admin, registro_Task, registro_User
     });
 
-    describe('Register/emit Events', function() {
+    describe('Register/emit Events: Facade', function() {
         var flag;
         var props;
         var service_expected;
@@ -62,7 +56,7 @@ describe('Test: module AppContext', function() {
 
         AppContext.onEvent('register', register);
         AppContext.onEvent('addCache', addCache);
-        AppContext.onEvent('newSession', newSession, 'registro_User');
+        AppContext.onEvent('newSession', newSession);
 
         AppContext.emitEvent('register');
         it('events existent', function() {
@@ -77,7 +71,7 @@ describe('Test: module AppContext', function() {
         });
 
         var session = {id: 47385932, value: {username: 'Maxfaider', topic: ['programing', 'chess', 'novelist']}};
-        AppContext.emitEvent('newSession', session);
+        AppContext.emitEvent('newSession', session, 'registro_User');
         it('events existent with properties and services', function() {
             assert.strictEqual(session_expected, session);
             assert.ok(service_expected);
